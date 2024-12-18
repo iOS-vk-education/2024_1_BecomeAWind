a060c68cfd7bf181a2520fb7960c1bc692b3c93b
Arsalan <batyavzdanii749@gmail.com>
2024-12-18 19:31:17 +0300

    обновил версию ios

---
+++ Yam/Yam/ViewDetalInformation/ScreenViews/HeadingView.swift
@@ -0,0 +1,18 @@
+//
+//  HeadingView.swift
+//  Yam
+//
+//  Created by Ширапов Арсалан on 18.12.2024.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeadingView()
+}
