a3f726ecaf24870f51880a594570bed8f8233928
Arsalan <batyavzdanii749@gmail.com>
2025-04-11 18:38:53 +0300

    ошибкa в TempDatabase

--- Yam/Yam/Core/TempDatabase.swift
+++ Yam/Yam/Core/TempDatabase.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 import MapKit
+import FirebaseCore
+import FirebaseFirestore
 
 protocol DatabaseDescription {
     func add(event: Event)
@@ -14,6 +16,7 @@ enum EventType {
 }
 
 final class TempDatabase: ObservableObject {
+    private let db = Firestore.firestore()
 
     static let shared = TempDatabase()
 
@@ -22,71 +25,94 @@ final class TempDatabase: ObservableObject {
     private var allEvents: [Event] = []
 
     private init() {
-        generateEvents()
+//        generateEvents()
         Logger.databaseInit()
+    }
+    
+    enum TempDatabaseError: Error {
+        case invalidDocument
+    }
+    
+    func loadEvents(completion: @escaping ([Event]?, Error?) -> Void) {
+        db.collection("events").getDocuments() { snapshot, error in
+            if let error {
+                completion(nil, error)
+                return
+            }
+            
+            var events = [Event]()
+            
+            snapshot.documents.forEach({ document in
+                if let event = Event(document.data()) {
+                    events.append(event)
+                }
+            })
+            
+            completion(events, nil)
+        }
     }
 
     private func generateEvents() {
-        let image1 = UIImage(named: "football")!
-        let image2 = UIImage(named: "projectx")!
-        let image3 = UIImage(named: "default_event_image")!
-        let image4 = UIImage(named: "chess")!
+//        let image1 = UIImage(named: "football")!
+//        let image2 = UIImage(named: "projectx")!
+//        let image3 = UIImage(named: "default_event_image")!
+//        let image4 = UIImage(named: "chess")!
+//
+//        let title1 = "матч в футбол 11 на 11"
+//        let title2 = "поход на концерт Нейромонаха Феофана"
+//        let title3 = "ищу человека для похода в кино на фильм ПРОРОК ищу"
+//        let title4 = "партия в шахматы"
+//
+//        let seats1 = Seats(busy: 0, all: 21)
+//        let seats2 = Seats(busy: 0, all: 3000)
+//        let seats3 = Seats(busy: 0, all: 1)
+//        let seats4 = Seats(busy: 0, all: 1)
+//
+//        let link1 = "https://github.com/ilyansky/born2code"
+//        let link2 = "https://github.com/ilyansky/VKTestTask"
+//        let link3 = "\n😄\t😄\0\r"
+//        let link4 = "https://github.com/ilyansky/born2code"
+//
+//        let date = Date()
+//
+//        let geopoint1 = CLLocation(latitude: 50.8651, longitude: 6.5811)
+//        let geopoint2 = CLLocation(latitude: 52.0571, longitude: 5.2405)
+//        let geopoint3 = CLLocation(latitude: 44.5782, longitude: 13.2687)
+//        let geopoint4 = CLLocation(latitude: 44.5782, longitude: 44.5782)
+//
+//        let place1 = Place(
+//            location: geopoint1,
+//            placeDescription: "Германия\nКерпен\nHeribertstraße\n37\n\nШирота: 50.8651\nДолгота: 6.5811"
+//        )
+//        let place2 = Place(
+//            location: geopoint2,
+//            placeDescription: "Kromme Rijn\n\nШирота: 52.0571\nДолгота: 5.2405"
+//        )
+//        let place3 = Place(
+//            location: geopoint3,
+//            placeDescription: "Северный Атлантический океан\n\nШирота: 44.5782\nДолгота: 13.2687"
+//        )
+//        let place4 = Place(
+//            location: geopoint4,
+//            placeDescription: "Северный Атлантический океан\n\nШирота: 44.5782\nДолгота: 44.5782"
+//        )
 
-        let title1 = "матч в футбол 11 на 11"
-        let title2 = "поход на концерт Нейромонаха Феофана"
-        let title3 = "ищу человека для похода в кино на фильм ПРОРОК ищу"
-        let title4 = "партия в шахматы"
-
-        let seats1 = Seats(busy: 0, all: 21)
-        let seats2 = Seats(busy: 0, all: 3000)
-        let seats3 = Seats(busy: 0, all: 1)
-        let seats4 = Seats(busy: 0, all: 1)
-
-        let link1 = "https://github.com/ilyansky/born2code"
-        let link2 = "https://github.com/ilyansky/VKTestTask"
-        let link3 = "\n😄\t😄\0\r"
-        let link4 = "https://github.com/ilyansky/born2code"
-
-        let date = Date()
-
-        let geopoint1 = CLLocation(latitude: 50.8651, longitude: 6.5811)
-        let geopoint2 = CLLocation(latitude: 52.0571, longitude: 5.2405)
-        let geopoint3 = CLLocation(latitude: 44.5782, longitude: 13.2687)
-        let geopoint4 = CLLocation(latitude: 44.5782, longitude: 44.5782)
-
-        let place1 = Place(
-            location: geopoint1,
-            placeDescription: "Германия\nКерпен\nHeribertstraße\n37\n\nШирота: 50.8651\nДолгота: 6.5811"
-        )
-        let place2 = Place(
-            location: geopoint2,
-            placeDescription: "Kromme Rijn\n\nШирота: 52.0571\nДолгота: 5.2405"
-        )
-        let place3 = Place(
-            location: geopoint3,
-            placeDescription: "Северный Атлантический океан\n\nШирота: 44.5782\nДолгота: 13.2687"
-        )
-        let place4 = Place(
-            location: geopoint4,
-            placeDescription: "Северный Атлантический океан\n\nШирота: 44.5782\nДолгота: 44.5782"
-        )
-
-        let event1 = Event(image: image1, title: title1, seats: seats1, link: link1, date: date, place: place1)
-        let event2 = Event(image: image2, title: title2, seats: seats2, link: link2, date: date, place: place2)
-        let event3 = Event(image: image3, title: title3, seats: seats3, link: link3, date: date, place: place3)
-        let event4 = Event(image: image4, title: title4, seats: seats4, link: link4, date: date, place: place4)
+//        let event1 = Event(image: image1, title: title1, seats: seats1, link: link1, date: date, place: place1)
+//        let event2 = Event(image: image2, title: title2, seats: seats2, link: link2, date: date, place: place2)
+//        let event3 = Event(image: image3, title: title3, seats: seats3, link: link3, date: date, place: place3)
+//        let event4 = Event(image: image4, title: title4, seats: seats4, link: link4, date: date, place: place4)
 
 
-        myEvents.append(event1)
-        myEvents.append(event2)
-
-        subscriptions.append(event4)
-        subscriptions.append(event3)
-
-        allEvents.append(event1)
-        allEvents.append(event2)
-        allEvents.append(event4)
-        allEvents.append(event3)
+//        myEvents.append(event1)
+//        myEvents.append(event2)
+//
+//        subscriptions.append(event4)
+//        subscriptions.append(event3)
+//
+//        allEvents.append(event1)
+//        allEvents.append(event2)
+//        allEvents.append(event4)
+//        allEvents.append(event3)
     }
 
 }
--- Yam/Yam/Modules/MakeEvent/Model/Event.swift
+++ Yam/Yam/Modules/MakeEvent/Model/Event.swift
@@ -3,10 +3,32 @@ import UIKit
 struct Event: Identifiable, Hashable {
     var id = UUID()
 
-    var image: UIImage
+    var image: UIImage?
+    let description: String?
     var title: String
     var seats: Seats
-    var link: String
-    var date: Date
+    var link: String?
+    var date: Date?
     var place: Place
+    
+//    init?(data: [String: Any] ) {
+//        guard
+//            let title = data["title"] as? String
+//        else {
+//            return nil
+//        }
+//        
+//        let body = data["description"] as? String
+//        let seats = data["seats"] as? Seats
+//        let place = data["place"] as? Place
+//        let link = data["link"] as? String
+//        
+//        
+//        
+//        self.title = title
+//        self.body = body
+//        self.seats = seats
+//        self.place = place
+//        self.link = link
+//    }
 }
